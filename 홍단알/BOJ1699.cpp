a8460745dfa24ab24aba19914d38c07d48c8b7b6
이채린 <67696767+cofls6581@users.noreply.github.com>
2021-10-24 10:01:03 +0900

    Create BOJ1699.cpp

---
+++ 홍단알/BOJ1699.cpp
@@ -0,0 +1,31 @@
+/*
+	BOJ1699
+	<제곱수의 합>
+	문제
+	어떤 자연수 N은 그보다 작거나 같은 제곱수들의 합으로 나타낼 수 있다. 예를 들어 11=32+12+12(3개 항)이다. 이런 표현방법은 여러 가지가 될 수 있는데, 11의 경우 11=22+22+12+12+12(5개 항)도 가능하다. 이 경우, 수학자 숌크라테스는 “11은 3개 항의 제곱수 합으로 표현할 수 있다.”라고 말한다. 또한 11은 그보다 적은 항의 제곱수 합으로 표현할 수 없으므로, 11을 그 합으로써 표현할 수 있는 제곱수 항의 최소 개수는 3이다.
+
+주어진 자연수 N을 이렇게 제곱수들의 합으로 표현할 때에 그 항의 최소개수를 구하는 프로그램을 작성하시오.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(NULL);
+	cin >> n;
+	vector <int> dp(n + 1);
+	for (int i = 1; i <= n; ++i) 
+		dp[i] = i;
+	dp[0] = 0;
+	for (int i = 4; i <= n; ++i) {
+		for (int j = 2; j*j <= i; ++j) {
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+	cout << dp[n];
+	return 0;
+}
