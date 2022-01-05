2fc27afe8f776d843ab8ad510525fdc091531bc1
이채린 <67696767+cofls6581@users.noreply.github.com>
2022-01-06 02:49:30 +0900

    Create 숫자 문자열과 영단어.cpp

---
+++ 프로그래머스/숫자 문자열과 영단어.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    for(int i=0;i<s.size();){
+        if(isdigit(s[i])) {
+            answer=answer*10+(s[i]-'0');
+            ++i;
+        }
+        else{
+            switch(s[i]){
+                case 'z':
+                    answer=answer*10;
+                    i+=4;
+                    break;
+                case 'o':
+                    answer=answer*10+1;
+                    i+=3;
+                    break;
+                case 't':
+                    if(s[i+1]=='w'){
+                        answer=answer*10+2;
+                        i+=3;
+                        break;
+                    }
+                    else{
+                        answer=answer*10+3;
+                        i+=5;
+                        break;
+                    }
+                case 'f':
+                    if(s[i+1]=='o'){
+                        answer=answer*10+4;
+                        i+=4;
+                        break;
+                    }
+                    else{
+                        answer=answer*10+5;
+                        i+=4;
+                        break;
+                    }
+                case 's':
+                    if(s[i+1]=='i'){
+                        answer=answer*10+6;
+                        i+=3;
+                        break;
+                    }
+                    else{
+                        answer=answer*10+7;
+                        i+=5;
+                        break;
+                    }
+                case 'e':
+                    answer=answer*10+8;
+                    i+=5;
+                    break;
+                default:
+                    answer=answer*10+9;
+                    i+=4;
+                    break;
+            }
+        }
+    }
+    
+    return answer;
+}
